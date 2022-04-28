b9fbec8bdd885a3bfa25ec3344ffaaeeae6c58b5
CHLOE JAGER <chloejager@chloes-mbp.attlocal.net>
2022-04-28 14:40:07 -0700

    got nav bar to stay on page when scrolling

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+
+
 function myFunction() {
     var x = document.getElementById("myNav");
     if (x.className === "nav") {
-      x.className += "responsive";
+      x.className += " responsive";
     } else {
       x.className = "nav";
     }
