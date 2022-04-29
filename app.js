17310d6044189aaf8a7deb80c54b1a7130f23109
CHLOE JAGER <chloejager@chloes-mbp.attlocal.net>
2022-04-29 09:23:57 -0700

    menu bar was not working for some reason

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 
-// function myFunction() {
-//     var x = document.getElementById("myNav");
-//     if (x.className === "nav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "nav";
-//     }
-//   }
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
 
 $(window).scroll(function() {
     $('html, body').animate({
